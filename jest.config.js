2be2a3950cf29c6dfc284b84557c704c139221fd
John Ollhorn <john@ollhorn.de>
2021-11-20 22:07:50 +0100

    Update

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 'use strict';
 
 const JestConfig = require('@bootcamp-project/jest-config/jest.svelte.config')
-module.exports = JestConfig
+module.exports = {
+    ...JestConfig,
+    coverageReporters: ['html','text'],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    }
+}
--- nyc.config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-const NYConfig = require('@bootcamp-project/istanbul-config/nyc.config')
-module.exports = NYConfig
