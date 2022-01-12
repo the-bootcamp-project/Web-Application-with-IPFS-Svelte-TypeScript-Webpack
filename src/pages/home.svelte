2e0d4ab9941d0744a13ded89206eae954364afa2
John Ollhorn <john@ollhorn.de>
2022-01-12 14:38:11 +0100

    Test (Button): Add Button Test in Home Component

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import Button from '@bootcamp-project/sveltelizer/button.svelte'
-	import H1 from '@bootcamp-project/sveltelizer/Typography/h1.svelte'
+	import H1 from '@bootcamp-project/sveltelizer/Typography/H1.svelte'
 	let name: string = 'world'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<H1>Hello {name}!</H1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<Button />
-	<H1>Test</H1>
 </main>
